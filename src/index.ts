acf7c773e016ab5b86bdcd3ab2dda68b84e4c1b1
Matheus Nunes <matheusnunesinfo@gmail.com>
2026-03-07 13:14:19 -0300

    refactor: migrate from Express to Fastify for GraphQL server integration
    - Updated context handling to use Fastify types and context function.
    - Replaced Express server setup with Fastify integration for Apollo Server.
    - Adjusted middleware and context building to align with Fastify's architecture.

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,29 @@
 import 'reflect-metadata'
 import { ApolloServer } from '@apollo/server'
-import { expressMiddleware } from '@as-integrations/express5'
-import express from 'express'
+import fastifyApollo from '@as-integrations/fastify'
+import fastify from 'fastify'
 import { buildSchema } from 'type-graphql'
-import { buildContext } from './graphql/context'
+import { buildContext, type GraphQLContext } from './graphql/context'
 import { resolvers } from './resolvers'
 
-async function bootstrap() {
-  const app = express()
+const app = fastify()
 
-  const schema = await buildSchema({
-    resolvers,
-    validate: false,
-    emitSchemaFile: './schema.graphql',
-  })
+const schema = await buildSchema({
+  resolvers,
+  validate: false,
+  emitSchemaFile: './schema.graphql',
+})
 
-  const server = new ApolloServer({
-    schema,
-    introspection: true,
-  })
-  await server.start()
+const server = new ApolloServer<GraphQLContext>({
+  schema,
+  introspection: true,
+})
+await server.start()
 
-  app.use(
-    '/graphql',
-    express.json(),
-    expressMiddleware(server, {
-      context: buildContext,
-    })
-  )
-  app.listen(4000, () => {
-    console.log('Server is running on http://localhost:4000/graphql')
-  })
-}
-
-bootstrap()
+app.register(fastifyApollo(server), {
+  path: '/graphql',
+  context: buildContext,
+})
+app.listen({ port: 4000 }, () => {
+  console.log('Server is running on http://localhost:4000/graphql')
+})
